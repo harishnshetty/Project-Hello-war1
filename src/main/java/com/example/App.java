576595c7c966a44cd4f7142a38fc6f1122c31dfe
harishnshetty <harishn662@gmail.com>
2025-05-16 20:09:16 +0530

    new files

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import com.google.gson.Gson;
+
+public class App {
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+        String json = gson.toJson("Hello, Maven!");
+        System.out.println(json);
+    }
+}
